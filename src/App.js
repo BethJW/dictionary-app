0fadff275dd76d6239ba014ea9741be1ecd6c58c
BethJW <bethanyjwilliamson16@gmail.com>
2024-02-04 20:24:49 +0000

    added alternative text to image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import logo from "./dictionary-image.png";
+import React from "react";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <span>
-          <img src={logo} className="logo" />
+          <img src={logo} className="logo" alt="logo" />
         </span>
         <span>
           <h1>Dictionary</h1>{" "}
