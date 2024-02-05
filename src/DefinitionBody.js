18e341a118946cd7a985874a69e7ff18c01cf929
BethJW <bethanyjwilliamson16@gmail.com>
2024-02-05 17:00:41 +0000

    fixing bugs in code

--- src/DefinitionBody.js
+++ src/DefinitionBody.js
@@ -4,10 +4,12 @@ export default function DefinitionBody(props) {
   return (
     <div className="definitionBody">
       <h2 className="word">
-        {" "}
         {props.data.word}{" "}
-        <span className="phonetic">{props.data.phonetic}</span>{" "}
+        <span className="phonetic">{props.data.phonetic}</span>
       </h2>
+      <div className="meaning">
+        <h3>{props.data.definitionObject}</h3>
+      </div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -4,9 +4,11 @@ import "./Search.css";
 import DefinitionBody from "./DefinitionBody";
 export default function Search() {
   let [Search, setSearch] = useState(null);
-  let [definitionData, setdefinitionData] = useState(null);
+  let [definitionData, setdefinitionData] = useState({ ready: false });
   function handleResponse(response) {
+    console.log(response);
     setdefinitionData({
+      ready: true,
       word: response.data[0].word,
       phonetic: response.data[0].phonetic,
       definitionObject: response.data[0].meanings[0].partOfSpeech,
@@ -19,29 +21,36 @@ export default function Search() {
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for ${Search}`);
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Search}`;
-    axios.get(apiUrl).then(handleResponse);
+    apiCall();
   }
   function handleChange(event) {
     setSearch(event.target.value);
   }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit} className="form mt-5 ">
-        <input
-          onChange={handleChange}
-          type="search"
-          placeholder="Search for a word..."
-          className="w-75 search-bar p-2 "
-        />
-        <input
-          type="submit"
-          value="search"
-          className="w-25 btn btn-outline-success p-2 search-btn"
-        />
-      </form>
-      <DefinitionBody data={definitionData} />
-    </div>
-  );
+  function apiCall() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Search}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (definitionData.ready) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit} className="form mt-5 ">
+          <input
+            onChange={handleChange}
+            type="search"
+            placeholder="Search for a word..."
+            className="w-75 search-bar p-2 "
+          />
+          <input
+            type="submit"
+            value="search"
+            className="w-25 btn btn-outline-success p-2 search-btn"
+          />
+        </form>
+        <DefinitionBody data={definitionData} />
+      </div>
+    );
+  } else {
+    apiCall();
+    return "Loading";
+  }
 }
